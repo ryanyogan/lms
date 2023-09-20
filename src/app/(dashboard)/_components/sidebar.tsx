9f9e77f752bc565329a92c86ec74fee6b412ecdb
Ryan Yogan <ryanyogan@gmail.com>
2023-09-19 21:12:01 -0500

    feat // sidebar and navbar basics

--- src/app/(dashboard)/_components/sidebar.tsx
+++ src/app/(dashboard)/_components/sidebar.tsx
@@ -4,7 +4,7 @@ import SidebarRoutes from "./sidebar-routes";
 export default function Sidebar() {
   return (
     <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
-      <div className="p-6">
+      <div className="p-7">
         <Logo />
       </div>
 
