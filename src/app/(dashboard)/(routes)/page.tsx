2a7baaafa5d9c4d11210dfef9532c510298bbe49
Ryan Yogan <ryanyogan@gmail.com>
2023-09-19 21:47:30 -0500

    feat // dynamic routes

--- src/app/(dashboard)/(routes)/page.tsx
+++ src/app/(dashboard)/(routes)/page.tsx
@@ -1,10 +1,3 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function DashboardPage() {
-  return (
-    <div>
-      This is a protected page
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
+  return <div>This is a protected page</div>;
 }
---
+++ src/app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -0,0 +1,3 @@
+export default function Analystics() {
+  return <div>Analystics</div>;
+}
